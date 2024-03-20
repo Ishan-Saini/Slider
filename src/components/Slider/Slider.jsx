f7c4b3a98b50ac76744c243bd41463fae8ba2a98
Ishan Saini <ishansaini243@gmail.com>
2024-03-20 16:21:19 +0530

    basic container setup

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Slider.module.css';
+
+const Slider = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.valueContainer}></div>
+    </div>
+  )
+}
+
+export default Slider;
